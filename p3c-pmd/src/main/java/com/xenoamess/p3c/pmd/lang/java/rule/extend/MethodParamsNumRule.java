8a1fa645d8ec7754c7e557d8c16fa3826948cd3c
Hubz <13941236135@163.com>
2023-07-22 13:58:10 +0800

    测试自定义规则

---
+++ p3c-pmd/src/main/java/com/xenoamess/p3c/pmd/lang/java/rule/extend/MethodParamsNumRule.java
@@ -0,0 +1,24 @@
+package com.xenoamess.p3c.pmd.lang.java.rule.extend;
+
+import com.xenoamess.p3c.pmd.lang.java.rule.AbstractAliRule;
+import net.sourceforge.pmd.lang.java.ast.ASTFormalParameters;
+
+/**
+ * @author hubz
+ * @date 2023/7/20 12:45
+ **/
+public class MethodParamsNumRule extends AbstractAliRule {
+ 
+    private static final int PARAMSNUM = 5;
+ 
+    @Override
+    public Object visit(ASTFormalParameters node, Object data) {
+        if (node.jjtGetNumChildren()>PARAMSNUM) {
+            addViolationWithMessage(data,node,"java.extend.MethodParamsNumRule.rule.msg");
+        };
+        return super.visit(node, data);
+    }
+ 
+}
+ 
+ 
