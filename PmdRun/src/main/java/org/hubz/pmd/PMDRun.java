1fa01186e877aca396d2b753d81060781482db07
HuBoZhi <1310225330@qq.com>
2023-07-24 17:38:08 +0800

    自定义启动命令

--- PmdRun/src/main/java/org/hubz/pmd/PMDRun.java
+++ PmdRun/src/main/java/org/hubz/pmd/PMDRun.java
@@ -1,8 +1,12 @@
 package org.hubz.pmd;
 
 import net.sourceforge.pmd.PMD;
+import org.apache.commons.cli.*;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author hubz
@@ -12,14 +16,63 @@ public class PMDRun {
 
     public static void main(String[] args) throws IOException {
         try {
-            String codeDir = "E:\\Coding\\SpringAll\\CheckStyleStudy";
-            String rulesets = "E:\\Coding\\SpringAll\\CheckStyleStudy\\ali-pmd.xml";
+            String path = "/pmd-rules.xml";
+            String filePath = ".\\rules\\";
+            String fileName = "pmd-rules.xml";
+            InputStream is = PMDRun.class.getResourceAsStream(path);
+            File file = new File(filePath + fileName);
+            if (!file.exists()) {
+                System.out.println("dasdadas");
+                FileUtils.copyInputStreamToFile(is, file);
+            }
+
+
+            Option codeDirOption = new Option("d", "codeDir", true, "code dir");
+            codeDirOption.setRequired(true);
+            Option rulesetsOption = new Option("rule", "rulesets", true, "rulesets file path");
+            rulesetsOption.setRequired(true);
+
+
+            Options options = new Options();
+            options.addOption(codeDirOption);
+            options.addOption(rulesetsOption);
+
+
+            CommandLine cli = null;
+            CommandLineParser cliParser = new DefaultParser();
+            HelpFormatter helpFormatter = new HelpFormatter();
+
+            try {
+                cli = cliParser.parse(options, args);
+            } catch (ParseException e) {
+                // 解析失败是用 HelpFormatter 打印 帮助信息
+                helpFormatter.printHelp(">>>>>> PMD Run options", options);
+                e.printStackTrace();
+            }
+
+            String codeDir = "";
+            String rulesets = "";
+            //根据不同参数执行不同逻辑
+            if (cli.hasOption("codeDir")) {
+                codeDir = cli.getOptionValue("codeDir");
+            } else if (cli.hasOption("d")) {
+                codeDir = cli.getOptionValue("d");
+            }
+            if (cli.hasOption("codeDir")) {
+                codeDir = cli.getOptionValue("codeDir");
+            } else if (cli.hasOption("d")) {
+                codeDir = cli.getOptionValue("d");
+            }
+
+            if ("".equals(codeDir)) {
+                helpFormatter.printHelp(">>>>>> PMD Run options", options);
+            }
+
             // 运行PMD
             PMD.runPmd("--dir", codeDir,
-                    "--format", "text",
                     "--rulesets", rulesets,
-                    "--encoding", "UTF-8",
-                    "--threads","5");
+                    "--format", "vbhtml",
+                    "--threads", "5");
         } catch (Exception e) {
             e.printStackTrace();
         }
